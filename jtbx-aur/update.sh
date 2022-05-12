41ae03254c2077a07e683f62248b08b0adf081b0
Jeremy Baxter <jeremytbaxter@protonmail.com>
2022-05-12 17:33:48 +1200

    Updated jtbx-aur/update.sh

--- jtbx-aur/update.sh
+++ jtbx-aur/update.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Update jtbx-aur repository script
 
+autocommit="true" # Automatically commit and push to GitHub?
+
 sudo printf "" # So you don't get prompted mid-way
 
 alias clone="git clone"
@@ -14,6 +16,7 @@ pkgmake() {
 mkdir sources
 cd sources
 clone https://aur.archlinux.org/librewolf-bin.git
+clone https://aur.archlinux.org/schildichat-desktop-bin.git
 clone https://aur.archlinux.org/lf.git
 clone https://aur.archlinux.org/nsxiv.git
 clone https://aur.archlinux.org/nerd-fonts-jetbrains-mono.git
@@ -25,8 +28,14 @@ sed -i 's/pkgname=librewolf-bin/pkgname=librewolf/' PKGBUILD
 sed -i '1 s/^/name=librewolf-bin\n/' PKGBUILD
 cd ..
 
+# SchildiChat didn't compile either, so I'm also patching that PKGBUILD
+cd schildichat-desktop-bin
+sed -i 's/pkgname=schildichat-desktop-bin/pkgname=schildichat-desktop/' PKGBUILD
+cd ..
+
 # Building packages; line 7
 pkgmake librewolf-bin
+pkgmake schildichat-desktop-bin
 pkgmake lf
 pkgmake nsxiv
 pkgmake nerd-fonts-jetbrains-mono
@@ -38,3 +47,6 @@ rm -rf sources/
 
 cd x86_64
 repo-add jtbx-aur.db.tar.xz *.pkg.tar.zst
+cd ../..
+
+[ "$autocommit" = "true" ] && git add * && git commit -m "[AutoCommit] Updated jtbx-aur repository" && git push
